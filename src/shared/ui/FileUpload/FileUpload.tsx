e458203438f278db2c137a7ae5c820f7335bd92e
keemsebeen <sebin0580@gmail.com>
2025-08-25 15:03:27 +0900

    feat: 파일 업로드 컴포넌트 구현

---
+++ src/shared/ui/FileUpload/FileUpload.tsx
@@ -0,0 +1,39 @@
+import { ComponentProps, useId } from 'react';
+
+import { Flex } from '../Flex';
+import { Icon } from '../Icon';
+import { Body3 } from '../Typography';
+
+export type FileUploadProps = {
+  /**
+   * The mode of the file upload.
+   */
+  mode: 'single' | 'multiple';
+} & ComponentProps<'input'>;
+
+export function FileUpload({ mode, ...props }: FileUploadProps) {
+  const inputId = useId();
+
+  return (
+    <label
+      className="focus-within:bg-primary-50 block w-full cursor-pointer rounded-xl border border-gray-200 bg-gray-50 px-4 py-3 transition-colors hover:bg-gray-100 md:py-3.5"
+      htmlFor={inputId}
+    >
+      <Flex alignItems="center">
+        <Icon name="file" size={25} color="gray" />
+        <Body3 className="text-gray-400" weight="medium">
+          {props.placeholder ||
+            (mode === 'single' ? '파일을 업로드해주세요' : '파일 여러개를 선택해주세요')}
+        </Body3>
+      </Flex>
+      <input
+        className="hidden"
+        id={inputId}
+        name="uploadFile"
+        type="file"
+        multiple={mode === 'multiple'}
+        {...props}
+      />
+    </label>
+  );
+}
