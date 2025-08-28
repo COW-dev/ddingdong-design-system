f773d331aa85d4dc3d7aacbf547864db5cc865e0
keemsebeen <sebin0580@gmail.com>
2025-08-29 02:28:29 +0900

    chore: index 추가, 버전 0.2.2 배포

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,11 +1,15 @@
 export { Accordion } from './ui/Accordion';
+export { Avatar } from './ui/Avatar/Avatar';
 export { Badge } from './ui/Badge';
 export { Button } from './ui/Button';
 export { Card } from './ui/Card';
 export { DoubleButton } from './ui/DoubleButton';
 export { Drawer } from './ui/Drawer';
+export { FileUpload } from './ui/FileUpload/FileUpload';
 export { Flex } from './ui/Flex';
 export { Icon } from './ui/Icon';
+export { IconButton } from './ui/IconButton';
+export { Input } from './ui/Input/Input';
 export { Modal } from './ui/Modal';
 export { NavBack } from './ui/NavBack';
 export { Pagination } from './ui/Pagination';
@@ -14,6 +18,8 @@ export { Select } from './ui/Select';
 export { Skeleton } from './ui/Skeleton';
 export { Switch } from './ui/Switch';
 export { Tabs } from './ui/Tabs';
+export { TextArea } from './ui/TextArea';
+export { Tooltip } from './ui/Tooltip';
 
 export * from './ui/Typography';
 export * from './ui/assets';
