a2ed46e5336c0c4a13486db2f88d6b1c3d84fc29
yougyung <youkyeongs@naver.com>
2025-11-14 16:28:38 +0900

    feat: add icon

--- src/shared/ui/assets/index.tsx
+++ src/shared/ui/assets/index.tsx
@@ -6,7 +6,9 @@ import ArrowUp from './arrow-up.svg';
 import Camera from './camera.svg';
 import Check from './check.svg';
 import Close from './close.svg';
+import Dots from './dots.svg';
 import DownLoad from './download.svg';
+import Dustpan from './dustpan.svg';
 import Etc from './etc.svg';
 import File from './file.svg';
 import List from './list.svg';
@@ -14,9 +16,13 @@ import Loading from './loading.svg';
 import Locate from './locate.svg';
 import NavbarArrow from './navbar-arrow.svg';
 import New from './new.svg';
+import Peoples from './peoples.svg';
 import Pin from './pin.svg';
+import PlusMinus from './plusminus.svg';
 import Refresh from './refresh.svg';
+import Report from './report.svg';
 import Search from './search.svg';
+import ShortReport from './shortReport.svg';
 import Trash from './trash.svg';
 import Upload from './upload.svg';
 import Write from './write.svg';
@@ -31,6 +37,12 @@ export const Icons = {
   close: Close,
   camera: Camera,
   download: DownLoad,
+  dots: Dots,
+  dustpan: Dustpan,
+  peoples: Peoples,
+  plusMinus: PlusMinus,
+  report: Report,
+  shortReport: ShortReport,
   etc: Etc,
   file: File,
   list: List,
