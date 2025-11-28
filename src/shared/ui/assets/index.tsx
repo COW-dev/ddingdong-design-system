b8c191af36fffe50089a180fb78450a776c64ef6
심유진 <syj11243@naver.com>
2025-11-29 01:48:11 +0900

    chore: score icon 추가

--- src/shared/ui/assets/index.tsx
+++ src/shared/ui/assets/index.tsx
@@ -23,6 +23,7 @@ import Pin from './pin.svg';
 import PlusMinus from './plusminus.svg';
 import Refresh from './refresh.svg';
 import Report from './report.svg';
+import Score from './score.svg';
 import Search from './search.svg';
 import ShortReport from './shortReport.svg';
 import Trash from './trash.svg';
@@ -60,6 +61,7 @@ export const Icons = {
   loading: Loading,
   refresh: Refresh,
   upload: Upload,
+  score: Score,
 };
 
 export type IconName = keyof typeof Icons;
